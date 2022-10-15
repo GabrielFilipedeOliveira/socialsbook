e8ac297b1b81e24976b968a2f3aa1df2b4c99a4d
GabrielFilipedeOliveira <110051377+GabrielFilipedeOliveira@users.noreply.github.com>
2022-10-15 11:12:57 -0300

    Update settings.py

--- social_book/settings.py
+++ social_book/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^&-7i615xn^9(f%^a=e8gqkoz4t28$^^g)d-#wy8psibhlkt#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
